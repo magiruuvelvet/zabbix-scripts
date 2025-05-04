3c1388a52715ff8530cbe975ecebaf166ac6ff50
magiruuvelvet <magiruuvelvet@naver.com>
2025-05-04 13:03:10 +0200

    yum-updates: fix update counter when no updates are available

--- sender_scripts/yum-updates.sh
+++ sender_scripts/yum-updates.sh
@@ -8,11 +8,11 @@ yum -q makecache
 
 # obtain a list of available package updates with their new version
 YUM_UPDATES_DETAILS="$(yum check-update | tail -n +3 | awk '{print $1 "=" $2}')"
-YUM_UPDATES_COUNT="$(echo "${YUM_UPDATES_DETAILS}" | wc -l)"
+YUM_UPDATES_COUNT="$(echo -n "${YUM_UPDATES_DETAILS}" | wc -l)"
 
 # build the stream for zabbix sender
 ZBX_STREAM=""
 ZBX_STREAM+="- yum.packagestoupdate.count ${YUM_UPDATES_COUNT}\n"
-ZBX_STREAM+="- yum.packagestoupdate.details \"$(echo "${YUM_UPDATES_DETAILS}" | tr "\n" " ")\"\n"
+ZBX_STREAM+="- yum.packagestoupdate.details \"$(echo "${YUM_UPDATES_DETAILS}" | tr "\n" " " | xargs)\"\n"
 
 echo -en "${ZBX_STREAM}" | zabbix_sender -c "/etc/zabbix/zabbix_agent2.conf" -i -
