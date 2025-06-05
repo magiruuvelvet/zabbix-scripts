6bf2da8d781bd2a55abc4b8270856ee4870d48cc
magiruuvelvet <magiruuvelvet@naver.com>
2025-06-05 10:31:14 +0200

    iptables-query: support ipv4 and ipv6

--- agent_scripts/iptables-query.sh
+++ agent_scripts/iptables-query.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+iptables_bin=
+
+case "$1" in
+  ipv4)
+    iptables_bin=iptables
+    ;;
+
+  ipv6)
+    iptables_bin=ip6tables
+    ;;
+
+  *) exit 1 ;;
+esac
+
 # print number of iptables rules (excluding sshguard blocked entries)
-echo -n $(iptables -S | grep -F -v -- '-A sshguard' | wc -l)
+echo -n $("$iptables_bin" -S | grep -F -v -- '-A sshguard' | wc -l)
